1e50310b29ae7329bdc3c4dbf665360dcfe5bf7a
Nicolas Modesto <modestonicolas02@gmail.com>
2025-10-15 21:13:50 -0300

    classe endereco

---
+++ model/enderecomodel.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Endereco:
+    id_endereco: int
+    id_usuario: int
+    titulo: str
+    logradouro: str
+    numero: str
+    complemento: str
+    bairro: str
+    cidade: str
+    uf: str
+    cep: str
+    
