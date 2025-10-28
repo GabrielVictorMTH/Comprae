b1fc1e872fb5960968d72c8577968a3670f11849
Ricardo Maroquio <maroquio@gmail.com>
2025-10-28 18:33:39 -0300

    crud categorias

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ from routes.auth_routes import router as auth_router
 from routes.tarefas_routes import router as tarefas_router
 from routes.chamados_routes import router as chamados_router
 from routes.admin_usuarios_routes import router as admin_usuarios_router
+from routes.admin_categorias_routes import router as admin_categorias_router
 from routes.admin_configuracoes_routes import router as admin_config_router
 from routes.admin_backups_routes import router as admin_backups_router
 from routes.admin_chamados_routes import router as admin_chamados_router
@@ -134,6 +135,9 @@ logger.info("Router de chamados incluído")
 
 app.include_router(admin_usuarios_router, tags=["Admin - Usuários"])
 logger.info("Router admin de usuários incluído")
+
+app.include_router(admin_categorias_router, tags=["Admin - Categorias"])
+logger.info("Router admin de categorias incluído")
 
 app.include_router(admin_config_router, tags=["Admin - Configurações"])
 logger.info("Router admin de configurações incluído")
